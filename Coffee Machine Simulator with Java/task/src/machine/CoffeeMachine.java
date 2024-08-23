f5d4e36cdac6972e5141b8f19f51de48e7cd8ed3
dschmerlaib <46738253+dschmerlaib@users.noreply.github.com>
2024-08-23 07:53:27 +0200

    Finished stage 4

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -2,86 +2,139 @@ package machine;
 
 import java.util.*;
 
+class Coffee {
+    public int RequiredWater;
+    public int RequiredBeans;
+    public int RequiredMilk;
+    public int Costs;
+    public String Name;
+    public int OrderNumber;
+
+    public Coffee(String name, int requiredWater, int requiredBeans, int requiredMilk, int costs, int orderNumber) {
+        RequiredWater = requiredWater;
+        RequiredBeans = requiredBeans;
+        RequiredMilk = requiredMilk;
+        Costs = costs;
+        Name = name;
+        OrderNumber = orderNumber;
+    }
+}
+
+class Machine {
+    public int Water = 400;
+    public int Milk = 540;
+    public int Beans = 120;
+    public int Money = 550;
+    public int DisposableCups = 9;
+
+    public HashMap<String, Coffee> Assortment;
+    private Scanner scanner;
+
+    public Machine(Scanner scanner) {
+        this.scanner = scanner;
+        this.Assortment = new HashMap<String, Coffee>();
+        this.Assortment.put("espresso", new Coffee("espresso", 250, 16, 0, 4, 1));
+        this.Assortment.put("latte", new Coffee("latte", 350, 20, 75, 7, 2));
+        this.Assortment.put("cappuccino", new Coffee("cappuccino", 200, 12, 100, 6, 3));
+    }
+
+    public void HandleAction() {
+        System.out.println("Write action (buy, fill, take): ");
+        String action = scanner.nextLine();
+
+        switch (action) {
+            case "buy":
+
+                Buy();
+
+                break;
+            case "fill":
+                Fill();
+                break;
+            case "take":
+                Take();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Buy() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        int selection = scanner.nextInt();
+        String selectionName = "";
+
+        switch (selection) {
+            case 1:
+                selectionName = "espresso";
+                break;
+            case 2:
+                selectionName = "latte";
+                break;
+            case 3:
+                selectionName = "cappuccino";
+                break;
+            default:
+                break;
+        }
+
+        Coffee coffee = Assortment.get(selectionName);
+
+        Water -= coffee.RequiredWater;
+        Milk -= coffee.RequiredMilk;
+        Beans -= coffee.RequiredBeans;
+        Money += coffee.Costs;
+        DisposableCups--;
+
+    }
+
+    public void Fill() {
+
+
+        System.out.println("Write how many ml of water you want to add:");
+        Water += scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        Milk += scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        Beans += scanner.nextInt();
+        System.out.println("Write how many disposable cups you want to add:");
+        DisposableCups += scanner.nextInt();
+    }
+
+    public void Take() {
+        System.out.println("I gave you $ " + Money);
+        Money = 0;
+    }
+
+    public void PrintStatus() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("The coffee machine has:\n");
+        builder.append(Water + " ml of water\n");
+        builder.append(Milk + " ml of milk\n");
+        builder.append(Beans + " g of coffee beans\n");
+        builder.append(DisposableCups + " disposable cups\n");
+        builder.append("$" + Money + " of money\n");
+
+        System.out.println(builder.toString());
+
+    }
+
+}
+
 public class CoffeeMachine {
-
-    public static ArrayList<String> steps = new ArrayList<String>();
-
-    private static int Water = 0;
-    private static int Milk = 0;
-    private static int Beans = 0;
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-
-        GetIngredientsAmount(scanner);
-        MakeCoffeeIfPossible(scanner);
-    }
-
-    private static void MakeCoffeeIfPossible(Scanner scanner) {
-
-        int possibleCoffees = PossibleAmountOfCoffees();
-
-        System.out.println("Write how many cups of coffee you will need:");
-        int numberOfCoffees = scanner.nextInt();
-
-
-
-        if (numberOfCoffees == possibleCoffees) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (numberOfCoffees < possibleCoffees) {
-            int diff = possibleCoffees - numberOfCoffees;
-            System.out.println("Yes, I can make that amount of coffee (and even " + diff + " more than that)");
-        } else {
-            System.out.println("No, I can make only " + possibleCoffees + " cup(s) of coffee");
-        }
-
+        Machine machine = new Machine(new Scanner(System.in));
+        machine.PrintStatus();
+        machine.HandleAction();
+        machine.PrintStatus();
 
     }
-
-    private static void GetIngredientsAmount(Scanner scanner) {
-
-        System.out.println("Write how many ml of water the coffee machine has:");
-        Water = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        Milk = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        Beans = scanner.nextInt();
-
-
-
-    }
-
-    private static int ValidateWater() {
-        final int waterPerCoffee = 200; // ml
-        return Water / waterPerCoffee;
-    }
-
-    private static int ValidateMilk() {
-        final int milkPerCoffee = 50; // ml
-        return Milk / milkPerCoffee;
-    }
-
-    private static int ValidateBeans() {
-        final int beansPerCoffee = 15; //grains
-        return Beans / beansPerCoffee;
-    }
-
-
-    private static int PossibleAmountOfCoffees() {
-
-        int water = ValidateWater();
-        int milk = ValidateMilk();
-        int beans = ValidateBeans();
-
-        int res = Math.min(water, Math.min(milk, beans));
-
-        return res;
-
-    }
-
-
+    
 }
 
 
